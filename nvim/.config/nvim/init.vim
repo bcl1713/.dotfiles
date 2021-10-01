b3ff22d011c9da6e444c1121014c72aed6ecc656
Brian Lucas <bcl1713@gmail.com>
2021-10-01 14:20:24 +0000

    Initial commit

---
+++ nvim/.config/nvim/init.vim
@@ -0,0 +1,47 @@
+call plug#begin(stdpath('data') . '/plugged')
+
+	Plug 'gruvbox-community/gruvbox'
+	Plug 'mfussenegger/nvim-jdtls'
+        Plug 'hrsh7th/nvim-compe'
+        Plug 'onsails/lspkind-nvim'
+        Plug 'neovim/nvim-lspconfig'
+        Plug 'tami5/lspsaga.nvim'
+        Plug 'vim-airline/vim-airline'
+        Plug 'vim-airline/vim-airline-themes'
+        Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+        Plug 'nvim-lua/plenary.nvim'
+        Plug 'nvim-telescope/telescope.nvim'
+
+call plug#end()
+
+set number relativenumber
+set nohlsearch incsearch
+set colorcolumn=80
+set splitright
+set splitbelow
+set expandtab
+set autoindent
+set cindent
+set softtabstop=2 shiftwidth=2
+
+nnoremap <SPACE> <Nop>
+let mapleader=" "
+
+augroup jdtls_lsp
+    autocmd!
+    autocmd FileType java lua require'java'.setup()
+augroup end
+
+"set background=dark
+colorscheme gruvbox
+
+nnoremap <silent>gh <cmd>lua require'lspsaga.provider'.lsp_finder()<CR>
+nnoremap <silent><leader>ca <cmd>lua require('lspsaga.codeaction').code_action()<CR>
+vnoremap <silent><leader>ca :<C-U>lua require('lspsaga.codeaction').range_code_action()<CR>
+nnoremap <silent><leader>k <cmd>lua require('lspsaga.hover').render_hover_doc()<CR>
+nnoremap <silent>gs :Lspsaga signature_help<CR>
+nnoremap <leader>ff <cmd>Telescope find_files<CR>
+nnoremap <leader>fg <cmd>Telescope live_grep<CR>
+nnoremap <leader>fb <cmd>Telescope buffers<CR>
+nnoremap <leader>fh <cmd>Telescope help_tags<CR>
+nnoremap <leader>df <cmd> Telescope find_files hidden=true search_dirs=~/.config/nvim/<CR>
---
+++ nvim/.config/nvim/lua/java.lua
@@ -0,0 +1,104 @@
+local M = {}
+
+function M.setup()
+
+  local on_attach = function(clinet, bufnr)
+    require 'jdtls.setup'.add_commands();
+    require'compe'.setup {
+      enabled = true;
+      autocomplete = true;
+      debug = false;
+      min_length = 1;
+      preselect = 'enable';
+      trottle_time = 80;
+      source_timeout = 200;
+      incomplete_delay = 400;
+      max_abbr_width = 100;
+      max_kind_width = 100;
+      max_menu_width = 100;
+      documentation = true;
+
+      source = {
+        path = true;
+        buffer = true;
+        calc = true;
+        vsnip = false;
+        nvim_lsp = true;
+        nvim_lua = true;
+        spell = true;
+        tags = true;
+        snippets_nvim = false;
+        treesitter = true;
+      };
+    }
+    
+    require'lspkind'.init()
+    require'lspsaga'.init_lsp_saga()
+
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    local opts = { noremap=true, silent=true }
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references() && vim.cmd("copen")<CR>', opts)
+    buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    -- Java specific
+    buf_set_keymap("n", "<leader>di", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
+    buf_set_keymap("n", "<leader>dt", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
+    buf_set_keymap("n", "<leader>dn", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
+    buf_set_keymap("v", "<leader>de", "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", opts)
+    buf_set_keymap("n", "<leader>de", "<Cmd>lua require('jdtls').extract_variable()<CR>", opts)
+    buf_set_keymap("v", "<leader>dm", "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", opts)
+
+    buf_set_keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+    vim.api.nvim_exec([[
+      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+      augroup lsp_document_highlight
+        autocmd!
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]], false)
+
+  end
+
+  local root_markers = {'.git', 'mvnw', 'gradlew', 'project.root', 'pom.xml'}
+  local root_dir = require('jdtls.setup').find_root(root_markers)
+  local home = os.getenv('HOME')
+
+  local workspace_folder = home .. "/.workspace" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+
+  local config = {
+    flags = {
+      allow_incremental_sync = true,
+    };
+    on_attach = on_attach,
+  }
+
+  config.settings = {
+    java = {
+      signatureHelp = { enabled = true };
+    };
+  }
+  config.cmd = {'/home/brian/.cache/nvim/nvim_lsp/jdtls/launcher', workspace_folder}
+  config.on_attach = on_attach
+
+  require('jdtls').start_or_attach(config)
+
+end
+
+return M
