14f5a6dc66e6b9531836d0f02bd1999733c34946
Brian Lucas <bcl1713@gmail.com>
2021-10-18 16:51:10 +0200

    add dotfiles telescope search remove ~ directory

--- nvim/.config/nvim/lua/nv-telescope/init.lua
+++ nvim/.config/nvim/lua/nv-telescope/init.lua
@@ -5,4 +5,5 @@ nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
 nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
 nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
 nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+nnoremap <leader>df <cmd>lua require('nv-telescope.custom').search_dotfiles()<cr>
 ]]
