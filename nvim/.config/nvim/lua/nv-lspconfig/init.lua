667f35b924ee89ca45df2ced3966f8178fdd0130
Brian Lucas <bcl1713@gmail.com>
2022-03-27 08:03:30 +0200

    Adds tsserver to lsp

--- nvim/.config/nvim/lua/nv-lspconfig/init.lua
+++ nvim/.config/nvim/lua/nv-lspconfig/init.lua
@@ -7,7 +7,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local lspconf = require "lspconfig"
-local servers = { "clangd" }
+local servers = { "clangd" , "pyright", "eslint", "tsserver"}
 
 -- Load language servers
 for _, lang in ipairs(servers) do
@@ -19,37 +19,37 @@ for _, lang in ipairs(servers) do
 end
 
 -- Lua LSP Specific
-USER = "/home/" .. vim.fn.expand("$USER")
-
-local sumneko_root_path = USER .. "/.config/lua-language-server"
-local sumneko_binary = USER .. "/.config/lua-language-server/bin/Linux/lua-language-server"
-
-lspconf.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-  root_dir = function()
-    return vim.loop.cwd()
-  end,
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-        path = vim.split(package.path, ";")
-      },
-      diagnostics = {
-        globals = {"vim"}
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-        }
-      },
-      telemetry = {
-        enable = false
-      }
-    }
-  }
-}
+-- USER = "/home/" .. vim.fn.expand("$USER")
+-- 
+-- local sumneko_root_path = USER .. "/.config/lua-language-server"
+-- local sumneko_binary = USER .. "/.config/lua-language-server/bin/Linux/lua-language-server"
+-- 
+-- lspconf.sumneko_lua.setup {
+--   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+--   root_dir = function()
+--     return vim.loop.cwd()
+--   end,
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         version = "LuaJIT",
+--         path = vim.split(package.path, ";")
+--       },
+--       diagnostics = {
+--         globals = {"vim"}
+--       },
+--       workspace = {
+--         library = {
+--           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+--           [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+--         }
+--       },
+--       telemetry = {
+--         enable = false
+--       }
+--     }
+--   }
+-- }
 
 vim.fn.sign_define("LspDiagnosticsSignError", {text = ""})
 vim.fn.sign_define("LspDiagnosticsSignWarning", {text = ""})
