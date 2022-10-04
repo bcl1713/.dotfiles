e39dfebfc9cd7f17c95ee9ea8c314e76e604a44d
Brian Lucas <bcl1713@gmail.com>
2022-10-04 12:49:19 +0200

    feat(luasnip): add luasnip and keybindings

--- nvim/.config/nvim/after/plugin/luasnip.rc.lua
+++ nvim/.config/nvim/after/plugin/luasnip.rc.lua
@@ -1,1 +1,39 @@
-require("luasnip.loaders.from_vscode").lazy_load()
+local status, ls = pcall(require, "luasnip")
+if (not status) then
+  print "luasnip not installed"
+  return
+end
+
+ls.config.set_config {
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
+}
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+
+ls.add_snippets("all", {
+  s("expand", t("--This is expanded")),
+}, {
+    key = "all"
+  })
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
+end, { silent = true }
+)
+
+vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.rc.lua<CR>")
